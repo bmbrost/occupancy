281cb898df55075091a626f7ebfa6c381d590730
bmbrost <bmbrost@gmail.com>
2016-09-10 19:10:59 -0600

    simplified code

--- fp/occ.fp.2.mcmc.R
+++ fp/occ.fp.2.mcmc.R
@@ -21,14 +21,17 @@ occ.fp.2.mcmc <- function(Y,W,X,priors,start,tune,n.mcmc,adapt=TRUE){
 	}
 
 	get.tune <- function(tune,keep,k,target=0.44){  # adaptive tuning
-		# a <- min(0.01,1/sqrt(k))
-		a <- min(0.025,1/sqrt(k))
+		a <- min(0.01,1/sqrt(k))
+		# a <- min(0.025,1/sqrt(k))
 		exp(ifelse(keep<target,log(tune)-a,log(tune)+a))
 	}
 
-	y.loglik <- function(y,p,phi){
-		(1-phi)*p^y*(1-p)^(1-y)+phi*y
+	y.lik <- function(y,p,phi,log=FALSE){
+		tmp <- (1-phi)*p^y*(1-p)^(1-y)+phi*y
+		if(log) tmp <- log(tmp)
+		tmp
 	}
+
 	
 	###
 	###  Create variables 
@@ -121,15 +124,14 @@ occ.fp.2.mcmc <- function(Y,W,X,priors,start,tune,n.mcmc,adapt=TRUE){
 		z1 <- z==1	
 	  	alpha.star <- rnorm(qW,alpha,tune$alpha)
 	  	p.star <- apply(W,3,function(x) expit(x%*%alpha.star))	 	
-	 	mh.star <- sum(log((1-phi)*dbinom(Y[z1,],1,p.star[z1,])+phi*Y[z1,]))
+	 	mh.star <- sum(y.lik(Y[z1,],p.star[z1,],phi,log=TRUE))
 	 		+sum(dnorm(alpha.star,mu.alpha,sigma.alpha,log=TRUE))
-	 		# sum(log((1-phi)*p.star[z1,]^Y[z1,]*(1-p.star[z1,])^(1-Y[z1,])
-	 			# +phi*Y[z1,]))
-	 		# sum(dbinom(Y[z1,],1,p.star[z1,],log=TRUE))
-	 	mh.0 <- sum(log((1-phi)*dbinom(Y[z1,],1,p[z1,])+phi*Y[z1,]))
+			# sum(log((1-phi)*dbinom(Y[z1,],1,p.star[z1,])+phi*Y[z1,]))
+	 		# sum(log((1-phi)*p.star[z1,]^Y[z1,]*(1-p.star[z1,])^(1-Y[z1,])+phi*Y[z1,]))
+	 	mh.0 <-	sum(y.lik(Y[z1,],p[z1,],phi,log=TRUE))
 	 		+sum(dnorm(alpha,mu.alpha,sigma.alpha,log=TRUE))
+			# sum(log((1-phi)*dbinom(Y[z1,],1,p[z1,])+phi*Y[z1,]))
 	 		# sum(log((1-phi)*p[z1,]^Y[z1,]*(1-p[z1,])^(1-Y[z1,])+phi*Y[z1,]))
-	 		# sum(dbinom(Y[z1,],1,p[z1,],log=TRUE))
 		if(exp(mh.star-mh.0) > runif(1)){
 			alpha <- alpha.star
 			p <- p.star
@@ -142,16 +144,13 @@ occ.fp.2.mcmc <- function(Y,W,X,priors,start,tune,n.mcmc,adapt=TRUE){
 	  	###  Sample z 
 	  	###
 # browser()
-		p1 <- psi*apply((1-phi)*p^Y*(1-p)^(1-Y)+phi*Y,1,prod)
+		p1 <- psi*apply(y.lik(Y,p,phi),1,prod)
+		# p1 <- psi*apply((1-phi)*p^Y*(1-p)^(1-Y)+phi*Y,1,prod)
 		p0 <- (1-psi)*apply(phi^Y*(1-phi)^(1-Y),1,prod)
 		psi.tmp <- p1/(p1+p0)	
 		z <- rbinom(n,1,psi.tmp)
 
-		# p0.tmp <- psi*apply(p^Y*(1-p)^(1-Y),1,prod)
-		# psi.tmp <- p0.tmp/(p0.tmp+(1-psi))	
-		# z[y0] <- rbinom(n.y0,1,psi.tmp[y0])
-	
-	
+			
 		###
 	  	###  Save samples 
 	  	###
